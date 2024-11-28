c2e914212b29ef447a72a499537599484b9ff512
Ankit Yadav <py90398@gmail.com>
2024-11-28 11:25:10 +0530

    simple modification

--- BinaryTree/PreorderTree.java
+++ BinaryTree/PreorderTree.java
@@ -17,23 +17,23 @@ public class PreorderTree {
     static class MakeTree {
         static int idx = -1;
 
-        public static Node buildTree(int nodes[]) {
-            idx++;
-            if (nodes[idx] == -1)
-                return null;
-
-            Node newNode = new Node(nodes[idx]);
-            newNode.left = buildTree(nodes);
-            newNode.right = buildTree(nodes);
-
-            return newNode;
-        }
-    }
-
-    public static void main(String[] args) {
-        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
-        MakeTree tree = new MakeTree();
-        Node root = tree.buildTree(nodes);
+        public Node buildTree(int nodes[]) {
+                    idx++;
+                    if (nodes[idx] == -1)
+                        return null;
+        
+                    Node newNode = new Node(nodes[idx]);
+                    newNode.left = buildTree(nodes);
+                    newNode.right = buildTree(nodes);
+        
+                    return newNode;
+                }
+            }
+        
+            public static void main(String[] args) {
+                int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+                MakeTree tree = new MakeTree();
+                Node root = tree.buildTree(nodes);
         System.out.println("root = " + root.data);
     }
 }
