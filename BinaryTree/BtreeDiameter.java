2653d349f49404cc78db88c08dcb8a97e46e8ef5
Ankit Yadav <py90398@gmail.com>
2024-12-01 11:47:24 +0530

    reduced the time complexity for finding the diameter to O9n)

--- BinaryTree/BtreeDiameter.java
+++ BinaryTree/BtreeDiameter.java
@@ -22,7 +22,7 @@ public class BtreeDiameter {
 
         return Math.max(lh,rh)+1;
     }
-    
+    //approach 1
     static int diameter(Node root){
         if(root==null) return 0;
         
@@ -36,6 +36,28 @@ public class BtreeDiameter {
         
     }
 
+    //Approach 2
+    static class Info{
+        int diam;
+        int ht;
+
+        public Info(int diam,int ht){
+            this.diam = diam;
+            this.ht = ht;
+        }
+    }
+
+    static Info diameter2(Node root){
+        if(root == null) return new Info(0,0);
+
+        Info leftInfo = diameter2(root.left);
+        Info rightInfo = diameter2(root.right);
+
+        int dia = Math.max(Math.max(leftInfo.diam, rightInfo.diam), leftInfo.ht+rightInfo.ht + 1);
+        int height  = Math.max(leftInfo.ht, rightInfo.ht) + 1;
+        return new Info(dia,height);
+    }
+
     public static void main ( String args[]){
         //creating the binary tree
         Node root = new Node(1);
@@ -46,6 +68,12 @@ public class BtreeDiameter {
         root.right.right = new Node(6);
 
         int treeDia = diameter(root);
+        System.out.println("Approach 1");
         System.out.println("diameter of the binary tree : " +treeDia); 
+
+        Info temp = diameter2(root);
+        System.out.println("Approach 2");
+        System.out.println("diameter of binary tree :" + temp.diam);
+        System.out.println("Height of binary tree : " + temp.ht);
     }
 }
